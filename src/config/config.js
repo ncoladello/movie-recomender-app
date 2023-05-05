d0a9aa35c1d3034e761afa4faade34c6cff56252
Natanael Coladello <ncoladello@gmail.com>
2023-05-05 20:58:39 +0200

    .

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-export const tmdbURL = "https://api.themoviedb.org/3/";
-export const tmdbAPIKey = '8227619f52e5b122d9d37744e84dfeb3';
-export const ipApiURL = "https://ipapi.co/json/";
-/*export const streamingAPIURL = "https://streaming-availability.p.rapidapi.com/search/basic";
-export const streamingAPIKey = 'efac9f0317msh6d938c68dc09e18p1a311bjsne6b19fce2372';*/
-
-
